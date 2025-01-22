12ce10ab071539c2eeb69eb88b580f2a94b84b55
khairulumam92 <khairulumamku92@gmail.com>
2025-01-22 16:43:54 +0700

    update

--- Calculator GPA/src/App.java
+++ Calculator GPA/src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args){
-        float SumGPA = 3.65;
+        double SumGPA = 3.65;
         System.out.println("=== GPA Calculator ===");
         System.out.println("number of courses: ");
-        System.out.println("Your GPA is: " + SumGPA());
+        System.out.println("Your GPA is: " + SumGPA);
     }
 }
